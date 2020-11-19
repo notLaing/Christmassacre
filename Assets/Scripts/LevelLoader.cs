90b59bb89e4f45eb1376cc6e912a14acba353162
Justin Lai <laij3@ferris.edu>
2020-11-19 18:04:49 -0500

    Merged. Screen transitions
    Merged with the assets from Meghan's push. Added screen transitioning: fade, circle wipe, pan. Currently have a filler asset for panning concept - to be replaced with backgrounds once parallax backgrounds are created

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = .1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            LoadNextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("Pressed G");
+            LoadGameOver();
+            Debug.Log("Check");
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(GameOver());
+    }
+
+    IEnumerator GameOver()
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene("GameOver");
+    }
+}
