ce4c14b9d6e129f0b23e46d97203adea7e044706
Justin Lai <laij3@ferris.edu>
2020-11-21 22:48:07 -0500

    Fixing merge conflicts
    Start point for merge conflict fixing. Had to push out new branch and mess with stuff in GitKraken to get out of detached head state and find my way back onto the main branch. Pushing to make sure this is the main branch.

---
+++ Assets/Scripts/Placeholder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeholder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is a placeholder script. I just need something to mess around with for gitkraken purposes. Nov. 21, 2020 at 10:44 pm
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
