7e825d0640319934205e5786db419025b8609fce
Justin Lai <laij3@ferris.edu>
2020-12-01 18:35:16 -0500

    Parallax background for Level 4
    Script is easy to set for anything

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float length = 0, startX;
+    public float parallaxEffect = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startX = transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        //move the background layer
+        transform.position = new Vector3(transform.position.x - (parallaxEffect * Time.fixedDeltaTime), transform.position.y, transform.position.z);
+
+        //reset position
+        if(transform.position.x < startX - length)
+        {
+            transform.position = new Vector3(transform.position.x + length, transform.position.y, transform.position.z);
+        }
+    }
+}
